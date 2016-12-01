72117b65f9d811b2cbb48a2a3ae86be4b79e4ba1
Allen Kenta Vu <kentavu@gmail.com>
2016-12-01 17:48:03 +0000

    added basic header parser function

--- headerParser.js
+++ headerParser.js
@@ -2,11 +2,13 @@ var express = require('express')
 
 var app = express()
 
-app.get('/:query', function(req, res) {
+app.get('/whoami', function(req, res) {
         
        //var data = req.params.query;
-        
-        //req.send(req.headers['x-forwarded-for']);
+        var ipAddress = req.headers['x-forwarded-for']
+        var systemInfo = req.headers['user-agent']
+        var languageInfo = req.headers["accept-language"]
+        res.send(req.headers["accept-language"]);
     });
 
 app.listen(8080, function () {
