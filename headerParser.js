0f425705caf469104e44a07bca33be343f3a4513
Allen Kenta Vu <kentavu@gmail.com>
2016-12-02 00:11:45 +0000

    changed to return the info more specifically

--- headerParser.js
+++ headerParser.js
@@ -1,16 +1,22 @@
-var express = require('express')
+var express = require('express');
 
-var app = express()
+var app = express();
 
 app.get('/whoami', function(req, res) {
         
        //var data = req.params.query;
-        var ipAddress = req.headers['x-forwarded-for']
-        var systemInfo = req.headers['user-agent']
-        var languageInfo = req.headers["accept-language"]
-        res.send(req.headers["accept-language"]);
+        var ipAddress = req.headers['x-forwarded-for'];
+        var systemInfo = req.headers['user-agent'];
+        var languageInfo = req.headers["accept-language"];
+        
+        var languageInfoArr = languageInfo.split(","); 
+        var systemInfoSpecific = systemInfo.substring(systemInfo.indexOf("(")+1, systemInfo.indexOf(")"));
+        
+        var dateObj = {"ipaddress": ipAddress, "language": languageInfoArr[0],"software": systemInfoSpecific};
+        //console.log(JSON.stringify(req.headers));
+        res.send(dateObj);
     });
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
-})
+});
