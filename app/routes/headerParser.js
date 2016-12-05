020844e8f7d4389c8c86a24a06a1f47b2eacc791
Allen Kenta Vu <kentavu@gmail.com>
2016-12-05 19:27:14 +0000

    added basic usage page +  heroku test

--- app/routes/headerParser.js
+++ app/routes/headerParser.js
@@ -2,6 +2,24 @@ var express = require('express');
 
 var app = express();
 
+module.exports = function(app) {
+    app.route('/whoami')
+        .get(function(req, res){
+            var ipAddress = req.headers['x-forwarded-for'];
+            var systemInfo = req.headers['user-agent'];
+            var languageInfo = req.headers["accept-language"];
+        
+            var languageInfoArr = languageInfo.split(","); 
+            var systemInfoSpecific = systemInfo.substring(systemInfo.indexOf("(")+1, systemInfo.indexOf(")"));
+        
+            var dateObj = {"ipaddress": ipAddress, "language": languageInfoArr[0],"software": systemInfoSpecific};
+            //console.log(JSON.stringify(req.headers));
+            res.send(dateObj);
+            
+        });
+
+};
+/*
 app.get('/whoami', function(req, res) {
         
        //var data = req.params.query;
@@ -20,3 +38,4 @@ app.get('/whoami', function(req, res) {
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
 });
+*/
---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+var port = process.env.PORT || 8080;
+var routes = require('./app/routes/headerParser.js');
+
+app.get('/', function(req, res) {
+  var fileName = path.join(__dirname, 'index.html');
+  res.sendFile(fileName, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    }
+    else {
+      console.log('Sent:', fileName);
+    }
+  });
+});
+
+routes(app);
+
+app.listen(port, function () {
+  console.log('Example app listening on port'  + port + '!')
+});
